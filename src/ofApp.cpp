a9e874950fe9a61ac2fcd9cebd440acad1c89957
Xavier Fischer <xavier@fischer.ninja>
2016-10-14 23:59:52 +0200

    filage ok

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -147,13 +147,14 @@ void ofApp::setupSceneManager() {
 	sceneManager.add(new ofxCity(IntToString(i++)));														postEnabledByScene[i - 1] = false;
 	sceneManager.add(new ofxCityPingPong(IntToString(i++)));														postEnabledByScene[i - 1] = false;
 	sceneManager.add(new ofxVideoScene("interlude.mov", IntToString(i++), false, false, false, false));	postEnabledByScene[i - 1] = false;
+	sceneManager.add(new ofxVideoScene("clouds.mov", IntToString(i++), false, true, false, true));	postEnabledByScene[i - 1] = false;
 	sceneManager.add(new ofxVideoScene("ginevra.mp4", IntToString(i++), false, false, false));	postEnabledByScene[i - 1] = false;
 	sceneManager.add(new ofxVideoMirror(IntToString(i++)));														postEnabledByScene[i - 1] = false;
 	sceneManager.add(new ofxVideoScene("deboval.mp4", IntToString(i++), false, false, false));	postEnabledByScene[i - 1] = false;
 	sceneManager.add(new ofxVideoScene("rigolo.mp4", IntToString(i++), false, false, false));	postEnabledByScene[i - 1] = false;
 	sceneManager.add(new ofxVideoScene("reve.mp4", IntToString(i++), false, false, false));	postEnabledByScene[i - 1] = false;
-	sceneManager.add(new ofxKinectMemory(IntToString(i++)));														postEnabledByScene[i - 1] = false;
 	//sceneManager.add(new ofxVideoScene("final.mov", IntToString(i++), false, false, false));	postEnabledByScene[i - 1] = false;
+	//sceneManager.add(new ofxVideoScene("bgloopblue.mov", IntToString(i++), false, false, false));	postEnabledByScene[i - 1] = false;
 
 	
 	/*sceneManager.add(new ofxOcean(IntToString(i++)));														postEnabledByScene[i-1] = false;

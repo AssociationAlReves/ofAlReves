1676031e26dfdd80c45ed27f03d5d8e5bba1f01d
Xavier Fischer <xavier.fischer@outlook.com>
2016-10-19 17:16:59 +0200

    removed ofxUI 2nd

--- src/ofxKinecticon.h
+++ src/ofxKinecticon.h
@@ -10,7 +10,7 @@
 #include "ofMain.h"
 #include "globals.h"
 #include "ofxAppUtils.h"
-#include "ofxUI.h"
+#include "ofxGui.h"
 #include "ofxOpenCv.h"
 #ifdef USE_KINECT
 #include "ofxKinect.h"
@@ -21,30 +21,33 @@
 #define VASA_KINECT_FRAMECOUNT 100
 #define VASA_KINECT_FRAMEDELAY 20
 
+#define VASA_KINECT_SETTINGS_FILE  "kinecticon_settings.xml"
+
 class ofxKinecticon : public ofxScene
 {
 public:
-	ofxKinecticon(string prefix = "scene") : ofxScene(prefix + ": " + "Kinecticon") {
-			setSingleSetup(false); // call setup each time the scene is loaded
-		}
-
-	void setup();
-	void update();
-	void draw();
-	void drawPointCloud();
-	void exit();
-
-	void keyPressed(int key);
-
-	// ---------------------------------------------
+    ofxKinecticon(string prefix = "scene") : ofxScene(prefix + ": " + "Kinecticon") {
+        setSingleSetup(false); // call setup each time the scene is loaded
+    }
+    
+    void setup();
+    void update();
+    void draw();
+    void drawPointCloud();
+    void exit();
+    
+    void keyPressed(int key);
+    
+    // ---------------------------------------------
     // kinect related suff
     bool kinectEnabled;
     bool bKinectSetup;
-	bool bShowHelp;
-
-	ofxUISuperCanvas *gui;
+    bool bShowHelp;
     
-	#ifdef USE_KINECT
+    ofxPanel gui;
+    bool bGuiInitialized;
+    
+#ifdef USE_KINECT
     ofxKinect kinect;
     vector<ofMesh> kinectMeshes; // meshes for kinect delayed point cloud (see https://vimeo.com/89527246 )
     vector<ofColor> kinectMeshColors;
@@ -56,23 +59,23 @@ public:
     ofxCvGrayscaleImage liveImage; // grayscale depth image
     ofxCvGrayscaleImage grayThreshNear; // the near thresholded image
     ofxCvGrayscaleImage grayThreshFar; // the far thresholded image
-
+    
     ofxCvGrayscaleImage grayBgImage; // the average background image
     ofxCvGrayscaleImage grayDiff;
     ofxCvContourFinder contourFinder;
-	
+    
     
     bool bLearnBackground;
     int numFramesBg;
     bool bDrawPointCloud;
-    bool bKinectUseBg;
-    bool bKinectAltColor;
+    ofParameter<bool> bKinectUseBg;
+    ofParameter<bool> bKinectAltColor;
     
-    int nearThreshold;
-    int farThreshold;
-    int threshold;
+    ofParameter<int> nearThreshold;
+    ofParameter<int> farThreshold;
+    ofParameter<int> threshold;
     
     int angle;
-    #endif
+#endif
 };
 
